6fb054e86287f0a266242beff50924ecb2bb928a
Joseph Lenox <lenox.joseph@gmail.com>
2012-08-27 14:52:15 -0500

    Added state load functions and a subckt-specific save function

--- mergestate.cc
+++ mergestate.cc
@@ -1,34 +1,56 @@
 #include "mergestate.h"
-
+using namespace std;
 // Writes the introductory header
 inline void merge_header(FILE* file, const size_t count) {
 	fwrite(&count, sizeof(size_t), 1, file);
 }
 // Saves a single array to a file.
-void merge_write(FILE* file, const uint32_t* array, const size_t length) {
+void merge_write(FILE* file, const int32_t* array, const size_t length) {
 	fwrite(&length, sizeof(size_t), 1, file);
-	fwrite(array, sizeof(uint32_t), length, file);
+	fwrite(array, sizeof(int32_t), length, file);
 }
 // Reads an array from a file, allocating it. 
-void merge_read(FILE* file, uint32_t** array) {
-	uint64_t length;
+void merge_read(FILE* file, int32_t** array) {
+	size_t length;
 	fread(&length, sizeof(size_t), 1, file); // get the count
-	*array = new uint32_t[length];
-	fread(array, sizeof(uint32_t), length, file);
+	*array = new int32_t[length];
+	fread(array, sizeof(int32_t), length, file);
 }
 
 // save a single 
-void merge_save(const char* filename, const uint32_t* src, const size_t width) {
+void merge_save(const char* filename, const int32_t* src, const size_t width) {
 	FILE* out = fopen(filename, "wb");
 	merge_header(out, 1);
 	merge_write(out, src, width);
 	fclose(out);
 }
-void merge_save(const char* filename, const std::vector<uint32_t*>& src, const std::vector<SubCkt>& sckts) {
+void merge_save(const char* filename, const std::vector<int32_t*>& src, const vector<SubCkt>& sckts) {
 	FILE* out = fopen(filename, "wb");
-	merge_header(out, 1);
-	merge_write(out, src, width);
+	size_t subid = 0;
+	merge_header(out, src.size());
+	for (vector<SubCkt>::const_iterator it = sckts.begin(); it < sckts.end(); it++)
+		merge_write(out, src.at(subid++), it->size());
 	fclose(out);
 }
-void merge_load() {
+void merge_load(const char* filename, std::vector<int32_t*>& dst) {
+	int32_t* temp_array;
+	FILE* in = fopen(filename,"rb");
+	size_t length;
+	fread(&length, sizeof(size_t), 1, in);
+	for (size_t i = 0; i < length; i++) {
+		temp_array = NULL;
+		merge_read(in, &temp_array);
+		dst.push_back(temp_array);
+	}
+}
+
+int32_t* merge_load(const char* filename) {
+	int32_t* temp_array;
+	FILE* in = fopen(filename,"rb");
+	size_t length;
+	fread(&length, sizeof(size_t), 1, in);
+	for (size_t i = 0; i < length; i++) {
+		merge_read(in, &temp_array);
+	}
+	return temp_array;
 }
--- mergestate.h
+++ mergestate.h
@@ -7,10 +7,12 @@
 #include <vector>
 #include "subckt.h"
 
-void merge_write(FILE* file, const uint32_t* array, const size_t length);
-void merge_read(FILE* file, uint32_t** array);
+void merge_write(FILE* file, const int32_t* array, const size_t length);
 void merge_header(FILE* file, const size_t count);
-void merge_save(const char* filename, const std::vector<uint32_t*>& src, const std::vector<SubCkt>& sckts);
-void merge_save(const char* filename, const uint32_t* src, const size_t width);
+void merge_save(const char* filename, const std::vector<int32_t*>& src, const std::vector<SubCkt>& sckts);
+void merge_save(const char* filename, const int32_t* src, const size_t width);
 
+void merge_read(FILE* file, int32_t** array);
+void merge_load(const char* filename, std::vector<int32_t*>& dst);
+int32_t* merge_load(const char* filename);
 #endif // MERGESTATE_H
