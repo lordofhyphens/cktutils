7c1ae078d979f41418b3102bb9e3612b60368c46
Joseph Lenox <lenox.joseph@gmail.com>
2013-08-13 23:23:23 -0500

    removed templat specializations, added hashkey union for generating hash.
    Signed-off-by: Joseph Lenox <lenox.joseph@gmail.com>

--- segment.h
+++ segment.h
@@ -19,28 +19,15 @@ typedef struct cache_b_t {
 	hashable_int2 key;
 } cache_b;
 
-
+template <int N>
+union hashkey {
+	unsigned int k[N];
+	unsigned char h[sizeof(unsigned int)*N];
+};
 /* Generic segment structure. Uses int2 for the pid, -/+ */
 template <int N>
 struct segment_t { 
 	uint32_t mutex;
-	int key[N];
+	hashkey<N> key;
 	int2 pid;
 };
-
-// Specialized templates to take advantage of inbuilt CUDA vector types.
-template<>
-struct segment_t<4> {
-	uint32_t mutex;
-	int4 nums;
-	__device__ __host__ inline int get(uint8_t id) { switch (id) { case 0: return nums.x; case 1: return nums.y; case 2: return nums.z; case 3: return nums.w;} return 0;};
-	__device__ __host__ inline void set(uint8_t id, const int& val) { switch (id) { case 0: nums.x = val; break; case 1: nums.y = val; break; case 2: nums.z = val; break; case 3: nums.w = val;} };
-};
-template<>
-struct segment_t<2> {
-	int2 nums;
-	uint32_t mutex;
-	__device__ __host__ inline int get(uint8_t id) { switch (id) { case 0: return nums.x; case 1: return nums.y;} return 0;};
-	__device__ __host__ inline void set(uint8_t id, const int& val) { switch (id) { case 0: nums.x = val; break; case 1: nums.y = val;} };
-};
-
