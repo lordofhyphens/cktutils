80841f78c264e72f0f8b59ca9bdeaa5e9d49a2b6
Joseph Lenox <lenox.joseph@gmail.com>
2015-05-29 19:29:20 -0500

    added copy constructor

--- ckt.h
+++ ckt.h
@@ -80,6 +80,9 @@ class Circuit {
 		void annotate(std::vector<NODEC>*);
 	public:
 		Circuit();
+    Circuit(const Circuit& other) :  graph(new std::vector<NODEC>(other.graph->begin(), other.graph->end(),other.graph->get_allocator())),  
+    name(other.name),
+      _avg_nfo(other._avg_nfo), _max_nfo(other._max_nfo), _levels(other._levels) { }
     void tweak(const int, int);
 		Circuit(int type, const char* benchfile) {
 			this->graph = new std::vector<NODEC>();
