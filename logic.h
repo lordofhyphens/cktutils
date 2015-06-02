93a3f6739e7fe7e037afea4e6f0e09a128b8b84d
Joseph Lenox <lenox.joseph@gmail.com>
2015-06-01 21:14:30 -0500

    added equivalancy to string, another logicblock, and a move constructor

--- logic.h
+++ logic.h
@@ -1,18 +1,42 @@
+#ifndef LOGIC_H
+#define LOGIC_H
 #include <string>
+#include <algorithm>
 #include <vector>
 
 using std::vector;
+using std::begin;
+using std::end;
+using std::move;
+using std::forward;
+
+enum class LogicType { And, Or, Not, Xor, Xnor, Buff, Inpt, DFF, DFF_in, Unknown};
+using logic_t = LogicType;
+
+using LogicType::Unknown;
 
 class LogicBlock
 {
   public:
-    LogicBlock(std::string n) : _name(n) { }
+    LogicBlock(std::string n) : type(Unknown), primary_out(false), _name(n) { }
+    LogicBlock(std::string n, logic_t type) : type(type), primary_out(false), _name(n) { }
     std::string name() const { return _name; }
     void add_fanin(const LogicBlock& o) { fin.push_back(o._name); }
 
     std::vector<std::string> fin;
 
-    inline size_t nfi() { return fin.size();}
+    inline size_t nfi() { return fin.size(); }
+    bool operator<(const LogicBlock& z) const { return std::find(begin(z.fin), end(z.fin), _name) != end(z.fin);}
+    bool operator==(const LogicBlock& z) const { return _name == z._name && fin == z.fin; }
+    bool operator==(const std::string& z) const { return _name == z; }
+
+    LogicBlock(LogicBlock&& other) : fin(move(other.fin)), type(move(other.type)), primary_out(move(other.primary_out)), _name(std::move(other._name)) 
+    { other.type = logic_t::Unknown; }
+
+    LogicType type;
+    bool primary_out;
   private:    
     const std::string _name;
 };
+
+#endif // LOGIC_H
--- tests/logic_test.cpp
+++ tests/logic_test.cpp
@@ -20,18 +20,36 @@ TEST_GROUP(LB_BASIC)
   void setup()
   {
     block1 = std::unique_ptr<LogicBlock>(new LogicBlock("G0"));
-    block2 = std::unique_ptr<LogicBlock>(new LogicBlock("G1"));
-    block3 = std::unique_ptr<LogicBlock>(new LogicBlock("G2"));
+    block2 = std::unique_ptr<LogicBlock>(new LogicBlock("G1", logic_t::Not));
+    block3 = std::unique_ptr<LogicBlock>(new LogicBlock("G2", logic_t::Buff));
     block2->add_fanin(*block1);
     block3->add_fanin(*block2);
+    block3->primary_out = true;
   }
 
   void teardown()
   {
+    block1 = nullptr;
+    block2 = nullptr;
+    block3 = nullptr;
   }
 };
 
-TEST(LB_BASIC, get_name) {
+TEST(LB_BASIC, constructor) 
+{
+  CHECK(Unknown == block1->type);
+  CHECK(logic_t::Not == block2->type);
+  CHECK(logic_t::Buff == block3->type);
+}
+
+TEST(LB_BASIC, primaryout)
+{
+  CHECK(block3->primary_out);
+  CHECK_FALSE(block1->primary_out);
+}
+
+TEST(LB_BASIC, get_name) 
+{
   CHECK_EQUAL("G0", block1->name());
   CHECK_EQUAL("G1", block2->name());
   CHECK_EQUAL("G2", block3->name());
@@ -45,4 +63,33 @@ TEST(LB_BASIC, fanins) {
 
   CHECK_EQUAL(1, block3->nfi());
   CHECK_EQUAL("G1", block3->fin.at(0));
+}
+
+TEST(LB_BASIC, ordering)
+{
+  CHECK(*block1 < *block2);
+  CHECK(*block2 < *block3);
+
+  CHECK_FALSE(*block1 < *block3);
+}
+
+TEST(LB_BASIC, equality)
+{
+  CHECK(*block1 == *block1);
+  CHECK_FALSE(*block2 == *block3);
+  CHECK_FALSE(*block1 == *block3);
+  CHECK_FALSE(*block2 == *block1);
+}
+
+TEST(LB_BASIC, move)
+{
+  LogicBlock t3 = std::move(*block3);
+
+  CHECK_EQUAL("G2", t3.name());
+  CHECK_EQUAL(1, t3.nfi());
+  CHECK_EQUAL("G1", t3.fin.at(0));
+
+  CHECK_EQUAL(0, block3->nfi());
+  CHECK(Unknown == block3->type);
+  CHECK(t3.primary_out);
 }
