db299aca4a2e539ba5a2da2e8544160795ba645d
Joseph Lenox <lenox.joseph@gmail.com>
2015-06-01 21:18:40 -0500

    stubbed basic circuit test

---
+++ tests/circuit_test.cpp
@@ -0,0 +1,16 @@
+#include "../circuit.h"
+
+// needed for CppUtest
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/TestOutput.h"
+
+TEST_GROUP(CKT_BASIC)
+{
+  
+  void setup()
+  {
+  }
+  void teardown()
+  {
+  }
+};
